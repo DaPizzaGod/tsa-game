3343589faafe3523c16e386985dcaf1bcfe77142
Da_Pizza <advaithyt123@gmail.com>
2026-01-04 17:30:43 -0500

    got basic jump to work

--- scenes/player/spring_mode.gd
+++ scenes/player/spring_mode.gd
@@ -1,8 +1,28 @@
 extends PlayerParent
 
+var jump_vel:= -800.0
+
 func _process(delta: float) -> void:
 	# Apply Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
+	# Jump on floor
+	if is_on_floor() and Input.is_action_just_pressed("shoot"):
+		velocity.y = jump_vel
+	
+	if not is_on_floor():
+		look_at(get_global_mouse_position())
+		if Input.is_action_just_pressed("shoot"):
+			var dir = (get_global_mouse_position() - position)
+			air_jump(dir)
+		
+	# Move
+
 	move_and_slide()
+
+func air_jump(dir):
+	var velx = dir.x 
+	var vely = dir.y
+	velocity.x = velx
+	velocity.y = vely
