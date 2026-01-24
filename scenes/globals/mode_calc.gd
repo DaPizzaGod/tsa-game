7afe76bb27077afb3c1e0d50dc07804a0b181832
Da_Pizza <advaithyt123@gmail.com>
2026-01-23 21:15:45 -0500

    puts menu on game

--- scenes/globals/mode_calc.gd
+++ scenes/globals/mode_calc.gd
@@ -2,6 +2,9 @@ extends Node
 
 var mode := "normal"
 var max_count := 2
+var switch_mode_menu:PackedScene = preload("res://scenes/ui/switch_mode_menu.tscn")
+var level:PackedScene = preload("res://scenes/levels/level.tscn")
+var menu
 
 var modes := ["normal", "stretch", "spring"]
 var count := 0
@@ -9,6 +12,12 @@ var count := 0
 func _process(_delta: float) -> void:
 	# When shift is pressed
 	if Input.is_action_just_pressed("switch mode"):
+		print("hello")
+		menu = switch_mode_menu.instantiate()
+		get_tree().get_root().add_child(menu)
+		
+		
+
 		if count != max_count:
 			count += 1
 		else:
