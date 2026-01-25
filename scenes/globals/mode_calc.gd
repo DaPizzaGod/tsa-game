fa2d0d3283cf7b57409b56d0ab178d3a10c20ed3
Da_Pizza <advaithyt123@gmail.com>
2026-01-25 17:53:50 -0500

    fixed queue_free()

--- scenes/globals/mode_calc.gd
+++ scenes/globals/mode_calc.gd
@@ -4,17 +4,23 @@ var mode := "normal"
 var max_count := 2
 var switch_mode_menu:PackedScene = preload("res://scenes/ui/switch_mode_menu.tscn")
 var level:PackedScene = preload("res://scenes/levels/level.tscn")
-var menu
-
+var menu: Control
 var modes := ["normal", "stretch", "spring"]
 var count := 0
 
 func _process(_delta: float) -> void:
 	# When shift is pressed
 	if Input.is_action_just_pressed("switch mode"):
-		print("hello")
+		Engine.time_scale = 0.2
 		menu = switch_mode_menu.instantiate()
 		get_tree().get_root().add_child(menu)
+		
+		var menu_stay := Timer.new()
+		add_child(menu_stay)
+		menu_stay.wait_time = .1
+		menu_stay.timeout.connect(_on_menu_stay_timeout)
+		menu_stay.one_shot = true
+		menu_stay.start()
 		
 		
 
@@ -24,3 +30,9 @@ func _process(_delta: float) -> void:
 			count = 0
 		mode = modes[count]
 		print(mode)
+
+func _on_menu_stay_timeout():
+	print(typeof(menu), menu)
+	menu.queue_free()
+	Engine.time_scale = 1
+	
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,49 @@
-extends Node
+extends Node2D
+	
+
+
+var new_player
+var normal: PackedScene = preload("res://scenes/player/normal_mode.tscn")
+var stretch: PackedScene = preload("res://scenes/player/stretch_mode.tscn")
+var spring: PackedScene = preload("res://scenes/player/spring_mode.tscn")
+
+
+func _ready() -> void:
+	$PlayerNode.add_child(normal.instantiate())
+
+	
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("switch mode"):
+		if ModeCalc.mode == "normal":
+			swap_player(normal)
+		elif ModeCalc.mode == "stretch":
+			swap_player(stretch)
+		elif ModeCalc.mode == "spring":
+			swap_player(spring)
+			
+	if ModeCalc.menu:
+		ModeCalc.menu.position = new_player.global_position
+
+		
+func swap_player(scene: PackedScene):
+	var old_player = get_tree().get_nodes_in_group("Players")[0]
+	var old_pos = old_player.global_position
+	var old_vel = old_player.velocity
+	
+	old_player.queue_free()
+	
+	new_player = scene.instantiate()
+	$PlayerNode.add_child(new_player)
+	new_player.global_position = old_pos
+	new_player.velocity = old_vel
+	
+	#Slow down
+	if new_player.has_meta("tween"):
+		new_player.get_meta("tween").kill()
+	
+	var tween := create_tween()
+	tween.tween_property(new_player, "velocity:x", 0.0, 1.0).set_ease(Tween.EASE_IN_OUT)
+	
+	
+func _on_shoot_hand():
+	print("shoot")
