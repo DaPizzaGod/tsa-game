12d1c03b8c7b46ad45b66b35783077839fe85602
Da_Pizza <advaithyt123@gmail.com>
2026-01-29 15:33:25 -0500

    stamina coin finished

--- scenes/objects/stamina_coin.gd
+++ scenes/objects/stamina_coin.gd
@@ -3,8 +3,10 @@ extends Node2D
 var stamina_add_val : int
 
 func _ready() -> void:
-	stamina_add_val = randi() % 5 + 3
+	stamina_add_val = randi_range(3, 6)
 	print(stamina_add_val)
+
+
 func _on_collect_zone_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Players"):
 		StaminaCalc.current_stamina += stamina_add_val
