fd1a3b328bfe26c7a3020448e01fcb4f18e57082
Da_Pizza <advaithyt123@gmail.com>
2026-01-28 22:02:17 -0500

    finished jump

--- scenes/player/normal_mode.gd
+++ scenes/player/normal_mode.gd
@@ -4,14 +4,17 @@ var jump_vel := -600.0
 var direction: float
 
 
+
+
+
 func _process(delta: float) -> void:
 	# Apply Gravity
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += get_grav(velocity) * delta
 	
 	# Jump
 	
-	if Input.is_action_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_vel
 		
 	if Input.is_action_just_released("jump") and velocity.y < 0: 
@@ -25,7 +28,8 @@ func _process(delta: float) -> void:
 		direction = max(direction - acc, -max_speed)
 		
 	else:
-		direction = lerp(direction, 0.0, 0.5)
+		direction = move_toward(direction, 0.0, acc)
+
 		
 	if Input.is_action_pressed("shoot"):
 		print(direction)
