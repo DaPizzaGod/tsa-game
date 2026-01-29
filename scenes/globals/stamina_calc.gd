c063add995cc8c64df7e2cd0c81d6c7c9dd716d1
Da_Pizza <advaithyt123@gmail.com>
2026-01-29 15:28:43 -0500

    adds random stamina

--- scenes/globals/stamina_calc.gd
+++ scenes/globals/stamina_calc.gd
@@ -4,11 +4,18 @@ var max_stamina:= 30
 var current_stamina: int
 var update_stamina:= false
 var respawn:= false
+var check_if_over := false
 
 func _ready() -> void: 
 	current_stamina = max_stamina
 
 func _process(_delta: float) -> void:
+	if check_if_over:
+		if current_stamina > max_stamina:
+			current_stamina = max_stamina
+		print(current_stamina)
+		check_if_over = false
+		update_stamina = true
 	
 	
 	if current_stamina <= 0:
--- scenes/objects/stamina_coin.gd
+++ scenes/objects/stamina_coin.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
+var stamina_add_val : int
 
-
+func _ready() -> void:
+	stamina_add_val = randi() % 5 + 3
+	print(stamina_add_val)
 func _on_collect_zone_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Players"):
+		StaminaCalc.current_stamina += stamina_add_val
+		StaminaCalc.check_if_over = true
 		queue_free()
