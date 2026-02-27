ff72d29b1fcfd8de2d104df9a4e7e77bb3639324
Da_Pizza <advaithyt123@gmail.com>
2026-02-27 18:11:24 -0500

    lantern_pos

--- scenes/globals/throw_calc.gd
+++ scenes/globals/throw_calc.gd
@@ -1,13 +1,24 @@
 extends Node
 var picked_up := false
 var player_pos: Vector2
-var lantern_holding
+var lantern_holding: RigidBody2D
 var throwing:= false
+var lantern_pos: Vector2
+var has_lantern := false
+
 
 func throw_lantern(throw_force, forward_dir):
-	throwing = true
-	lantern_holding.global_position = player_pos
-	lantern_holding.linear_velocity = throw_force * forward_dir
+	if has_lantern:
+		throwing = true
+		lantern_holding.global_position = player_pos
+		lantern_holding.linear_velocity = throw_force * forward_dir
 
-	await get_tree().create_timer(0.1).timeout
-	throwing = false
+		await get_tree().create_timer(0.1).timeout
+		throwing = false
+
+func _process(_delta: float) -> void:
+	if typeof(lantern_holding) == 24:
+		has_lantern = true
+		lantern_pos = lantern_holding.position
+	else:
+		has_lantern = false
