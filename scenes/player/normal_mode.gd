83ac6732db3f3215eb74523ae74b1a0658d3c7cc
Da_Pizza <advaithyt123@gmail.com>
2026-03-18 22:19:28 -0400

    some working in code

--- scenes/player/normal_mode.gd
+++ scenes/player/normal_mode.gd
@@ -7,12 +7,18 @@ var running_bonus := 400.0
 var jump_buffer := false
 var jump_available := true
 var coyote_time := 0.1
+@onready var animation_player: AnimationPlayer = $Sprite/AnimationPlayer
+@onready var sprite: Node2D = $Sprite
+@onready var right_foot: Sprite2D = $Sprite/Body/RightFoot
+@onready var left_foot: Sprite2D = $Sprite/Body/LeftFoot
 
 
 
 
 
 func _physics_process(delta: float) -> void:
+	
+	
 	queue_redraw()
 	update_player_pos()
 	var acc = accel * delta
@@ -38,6 +44,7 @@ func _physics_process(delta: float) -> void:
 		
 		if jump_available:
 			if Input.is_action_just_pressed("jump") or jump_buffer:
+				animation_player.play("jump")
 				velocity.y = jump_vel
 				jump_buffer = false
 				jump_available = false
@@ -47,13 +54,28 @@ func _physics_process(delta: float) -> void:
 			velocity.y = jump_vel / 4
 		# Left and Right
 		
+		if running:
+			animation_player.speed_scale = 2
+		else:
+			animation_player.speed_scale = 1
+		
 		if Input.is_action_pressed("right"):
+			sprite.scale.x = 1
 			direction = min(direction + acc, max_speed)
+			if is_on_floor():
+				animation_player.play("walking")
 		elif Input.is_action_pressed("left"):
+			sprite.scale.x = -1
+
 			direction = max(direction - acc, -max_speed)
-			
+			if is_on_floor():
+				animation_player.play("walking")
 		else:
 			direction = move_toward(direction, 0.0, acc)
+			if is_on_floor():
+				animation_player.play("idle")
+			else:
+				animation_player.play("jump")
 
 		velocity.x = direction
 		
