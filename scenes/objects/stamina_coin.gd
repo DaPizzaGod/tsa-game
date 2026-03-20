4e46c7b97061c94d2d81d1bcad2a3cc5828c4c0a
Da_Pizza <advaithyt123@gmail.com>
2026-03-20 11:41:12 -0400

    lots of assets and stamina berry

--- scenes/objects/stamina_coin.gd
+++ scenes/objects/stamina_coin.gd
@@ -1,9 +1,34 @@
 extends Node2D
 
 var stamina_add_val : int
+var going_up := true
+var spacing := 50
+var tween: Tween
+var base_y: float
+
+	
+
+func move():
+	while is_inside_tree():
+		tween = create_tween()
+		
+		if going_up:
+
+			tween.tween_property(self, "position:y", base_y - spacing, 0.75).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+		elif !going_up:
+
+			tween.tween_property(self, "position:y", base_y + spacing, 0.75).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+		
+		await  tween.finished
+		going_up = !going_up
+
+
 
 func _ready() -> void:
 	stamina_add_val = randi_range(3, 6)
+	base_y = position.y
+
+	move()
 
 
 func _on_collect_zone_body_entered(body: Node2D) -> void:
