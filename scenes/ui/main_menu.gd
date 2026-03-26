1be06b11e729aadbc23355038fe1b624b5ce2c8a
Da_Pizza <advaithyt123@gmail.com>
2026-03-25 20:03:36 -0400

    main menu done

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -4,3 +4,13 @@ extends Control
 func _on_button_pressed() -> void:
 	if LevelCalc.finishing_level:
 		LevelCalc.menu_to_first_level()
+
+
+
+func _ready() -> void:
+	while 1:
+		var tween := create_tween()
+		tween.tween_property($Background, "scale", Vector2(0.2, 0.2), 0.1)
+		await tween.finished
+		tween.tween_property($Background, "scale", Vector2(0.1, 0.1), 0.1)
+		await  tween.finished
